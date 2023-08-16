22c019188ffe8d78916d3e2df6c0faac617e848e
yujinNa <yujin950916@gmail.com>
2023-08-16 14:49:52 +0900

    get api btn

--- src/pages/components/index.tsx
+++ src/pages/components/index.tsx
@@ -2,52 +2,48 @@ import styled from "@emotion/styled";
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 
-export default function  Button() {
+export default function Button() {
+  // const [value, setValue] = useState('')
+  //
+  // function onClickApiBtn(){
+  //   axios.get('https://jsonplaceholder.typicode.com/photos')
+  //     .then(response => setValue(response.data))
+  //   console.log(setValue)
+  // }
+  const [value, setValue] = useState('')
 
-    const [posts, setPosts] = useState([])
+  async function onClickApiBtn() {
+    const response = await axios.get('/emon/score')
+    console.log(response.data)
+    setValue(response.data[0].title)
+    // const onChange = (e) => {
+    //   setText(e.target.value)
+  }
 
-
-
-        useEffect(()=>{
-            // axios({
-            //     method : 'GET',
-            //     url : 'https://jsonplaceholder.typicode.com/photos'
-            // }).then(response => setPosts(response.data))
-
-                axios.get('https://192.168.50.67:8080/emon/score?page=1')
-                    .then(response => setPosts(response.data))
-            // function ApiButton(){
-            // }
-            // console.log(ApiButton())
-
-        })
-
-        // useEffect(async()=> {
-        //     try {
-        //         const response = await axios.get('https://jsonplaceholder.typicode.com/photos')
-        //         setPosts(response.data)
-        //     }catch (error){
-        //         console.log(error)
-        //     }
-        // })
+  const onReset = () => {
+    setValue('')
+  }
 
 
-    return (
-            <YujinInner>
-                <YujinButton01>버튼 입니당</YujinButton01>
-                <ul>
-                    {posts.map(post => (
-                        <li key={post.id}>
-                            <div>{post.title}</div>
-                            <div><img src={post.thumbnailUrl}/></div>
-                        </li>
-                    ))}
-                </ul>
-                <YujinButton02>확인</YujinButton02>
-                <YujinButton03>버~튼</YujinButton03>
-                <YujinButton04>또버튼</YujinButton04>
-            </YujinInner>
-    )
+  return (
+    <YujinInner>
+      <YujinButton01 onClick={onClickApiBtn}>테스트버튼</YujinButton01>
+      <input value={value}/>
+      {/*<YujinButton01 onClick={onClickApiBtn}>버튼 입니당</YujinButton01>*/}
+      {/*<input type="text" value={value}/>*/}
+      <ul>
+        {/*{posts.map(post => (*/}
+        {/*    <li key={post.id}>*/}
+        {/*        <div>{post.title}</div>*/}
+        {/*        <div><img src={post.thumbnailUrl}/></div>*/}
+        {/*    </li>*/}
+        {/*))}*/}
+      </ul>
+      <YujinButton02>확인</YujinButton02>
+      <YujinButton03>버~튼</YujinButton03>
+      <YujinButton04>또버튼</YujinButton04>
+    </YujinInner>
+  )
 }
 const PostsBox = styled.div`
   width: 300px;
@@ -80,7 +76,7 @@ const YujinButton01 = styled.button`
   cursor: pointer;
   color: #222;
 `
-const YujinButton02 = styled.button `
+const YujinButton02 = styled.button`
   width: 50px;
   height: 30px;
   border: none;
@@ -89,22 +85,23 @@ const YujinButton02 = styled.button `
   border-radius: 4px;
   transition: .3s;
   cursor: pointer;
-  
+
   &:hover {
     background: #3733E9;
   }
+
   &:active {
     background: #2B28CA;
   }
 `
-const YujinButton03 = styled.button `
+const YujinButton03 = styled.button`
   width: 200px;
   height: 50px;
   background: #fff;
   color: #ED4D4D;
   border: none;
   position: relative;
-  
+
   &::before {
     content: '';
     display: block;
@@ -116,6 +113,7 @@ const YujinButton03 = styled.button `
     left: 0;
     transition: .3s;
   }
+
   &:hover::before {
     width: 100%;
   }

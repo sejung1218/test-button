1e576732bea5c63e5fde81fe054da5ee5ecb7309
sejung1218 <sejung1218@hanmail.net>
2023-08-18 11:41:53 +0900

    테스트버튼 작업중

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "http://192.168.50.67:8080/:path*"
-      }
-    ]
-  }
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: "/:path*",
+                // destination: "http://192.168.50.67:8080/:path*"
+                destination: "http://192.168.100.188:8080/:path*"
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
--- src/pages/button/index.tsx
+++ src/pages/button/index.tsx
@@ -1,88 +1,96 @@
 import styled from '@emotion/styled';
 import axios from 'axios';
-import { useState } from 'react';
-import { useQuery, useQueryClient } from 'react-query';
+import {useQuery, useQueryClient} from 'react-query';
+import {useFormik} from "formik";
+import * as yup from 'yup';
+import {useState} from "react";
 
 export default function Button() {
   const queryClient = useQueryClient();
-  // GET ----------------------------------------------------------------------------------------------
-  const testReactQueryGet01 = useQuery(
-    'testReactQueryGet01',
-    () =>
-      axios.get(
-        'https://api.bonobono.dev/api/v1/example/reactquery/test/data01'
-        // ,{params: { data: 'data' },}
-      ),
-    {
-      enabled: false, // 초기 렌더링 시에는 API 호출 비활성화
-    }
-    // .then((data) => data)
-  );
-  // console.log('test "GET" data  : ', testReactQueryGet.data?.data.data.test);
-  console.log('test "GET" data01  : ', testReactQueryGet01.data);
+  const [queryEnabled, setQueryEnabled] = useState(false);
 
-  const testReactQueryGet02 = useQuery(
-    'testReactQueryGet02',
-    () =>
-      axios.get(
-        'https://api.bonobono.dev/api/v1/example/reactquery/test/data02'
-      ),
+  const testEmonGet = useQuery(
+    'testEmonGet',
+    () => axios.get('/emon/score'),
     {
-      enabled: false,
+      enabled: queryEnabled,
     }
   );
-  console.log('test "GET" data02  : ', testReactQueryGet02.data);
-  // const message = testReactQueryGet01.data;
 
-  const handleButtonClick01 = () => {
-    queryClient.removeQueries('testReactQueryGet02');
-    testReactQueryGet01.refetch(); // 버튼 1을 클릭할 때 첫 번째 API를 호출하도록 refetch 메소드 호출
-  };
+  const validationSchema = yup.object().shape({
+    inputText: yup.string().required('This field is required'),
+  });
 
-  const handleButtonClick02 = async () => {
-    queryClient.removeQueries('testReactQueryGet01');
-    testReactQueryGet02.refetch();
+  const formik = useFormik({
+    initialValues: {
+      inputText: '',
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      try {
+        await axios.put('/emon/score', values);
+        queryClient.removeQueries('testEmonGet');
+        setQueryEnabled(true); // 활성화 상태 변경
+      } catch (error) {
+        console.error('Error submitting form:', error);
+      }
+    },
+  });
+
+  const handleGetButtonClick = () => {
+    setQueryEnabled(true); // 활성화 상태 변경
   };
 
   return (
-    <>
-      <YujinInner>
-        {/* 버튼 1 클릭 시 API 호출 */}
-        <YujinButton02 onClick={handleButtonClick01}>1</YujinButton02>
-        {/* 버튼 2 클릭 시 API 호출 */}
-        <YujinButton02 onClick={handleButtonClick02}>2</YujinButton02>
-        <YujinButton02>3</YujinButton02>
-        <YujinButton02>4</YujinButton02>
-        <YujinButton02>5</YujinButton02>
-        <YujinButton02>6</YujinButton02>
-        <YujinButton02>7</YujinButton02>
-        {/* <YujinButton03 onClick={handleMakeCat}>버~튼</YujinButton03> */}
-      </YujinInner>
-    </>
+    <form onSubmit={formik.handleSubmit}>
+      <TestButtonWrapper>
+        <TestPutButtonWrapper>
+          <StyledInput
+            type="text"
+            id="getText"
+            name="getText"
+            placeholder="Get Data"
+          />
+          <TestButton type="button" onClick={handleGetButtonClick}>GET</TestButton>
+        </TestPutButtonWrapper>
+        <TestPutButtonWrapper>
+          <StyledInput
+            type="text"
+            id="inputText"
+            name="inputText"
+            placeholder="Enter text"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.inputText}
+          />
+          {formik.touched.inputText && formik.errors.inputText && (
+            <ErrorText>{formik.errors.inputText}</ErrorText>
+          )}
+          <TestButton type="submit">PUT</TestButton>
+        </TestPutButtonWrapper>
+      </TestButtonWrapper>
+    </form>
   );
 }
-const YujinInner = styled.div`
-  width: 1200px;
+
+const TestButtonWrapper = styled.div`
   margin: 0 auto;
   padding: 40px 0;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   gap: 20px;
 `;
-const YujinButton01 = styled.button`
-  width: 300px;
-  height: 40px;
-  border: 1px solid #ddd;
-  background: #efefef;
-  border-radius: 4px;
-  font-size: 14px;
-  font-weight: 500;
-  cursor: pointer;
-  color: black;
-`;
-const YujinButton02 = styled.button`
-  width: 150px;
+
+const TestPutButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+`
+
+const TestButton = styled.button`
+  width: 180px;
   height: 30px;
   border: none;
   background: #4844ff;
@@ -99,27 +107,10 @@ const YujinButton02 = styled.button`
     background: #2b28ca;
   }
 `;
-const YujinButton03 = styled.button`
-  width: 200px;
-  height: 50px;
-  background: #fff;
-  color: #ed4d4d;
-  border: none;
-  position: relative;
-
-  &::before {
-    content: '';
-    display: block;
-    width: 0;
-    height: 2px;
-    background: #ed4d4d;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    transition: 0.3s;
-  }
-
-  &:hover::before {
-    width: 100%;
-  }
+const StyledInput = styled.input`
+  width: 180px;
+  height: 30px;
+`;
+const ErrorText = styled.div`
+  color: red;
 `;
